55d1fc9b9f2b25868bfe8159c1cf2b01535ac503
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2022-10-21 22:08:38 +0200

    🎉 Initial commit

---
+++ Enum/COM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheKey_v2.Enum
+{
+    enum COM
+    {
+        // QMK BACKLIGHT
+        id_qmk_backlight_brightness = 0x09,
+        id_qmk_backlight_effect = 0x0A,
+
+        // QMK RGBLIGHT
+        id_qmk_rgblight_brightness = 0x80,
+        id_qmk_rgblight_effect = 0x81,
+        id_qmk_rgblight_effect_speed = 0x82,
+        id_qmk_rgblight_color = 0x83,
+    }
+}
---
+++ Enum/LightMode.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheKey_v2.Enum
+{
+    public enum LightMode
+    {
+        Off = 0x00,
+        Solid = 0x01,
+        Breathing1 = 0x02,
+        Breathing2 = 0x03,
+        Breathing3 = 0x04,
+        Breathing4 = 0x05,
+        RainbowMood1 = 0x06,
+        RainbowMood2 = 0x07,
+        RainbowMood3 = 0x08,
+        RainbowSwrirl1 = 0x09,
+        RainbowSwrirl2 = 0x0A,
+        RainbowSwrirl3 = 0x0B,
+        RainbowSwrirl4 = 0x0C,
+        RainbowSwrirl5 = 0x0D,
+        RainbowSwrirl6 = 0x0E,
+        Snake1 = 0x0F,
+        Snake2 = 0x10,
+        Snake3 = 0x11,
+        Snake4 = 0x12,
+        Snake5 = 0x13,
+        Snake6 = 0x14,
+        Knight1 = 0x15,
+        Knight2 = 0x16,
+        Knight3 = 0x17,
+        Christmas = 0x18,
+        Gradient1 = 0x19,
+        Gradient2 = 0x1A,
+        Gradient3 = 0x1B,
+        Gradient4 = 0x1C,
+        Gradient5 = 0x1D,
+        Gradient6 = 0x1E,
+        Gradient7 = 0x1F,
+        Gradient8 = 0x20,
+        Gradient9 = 0x21,
+        Gradient10 = 0x22,
+        RGBTest = 0x23,
+        Alternating = 0x24
+    }
+}
