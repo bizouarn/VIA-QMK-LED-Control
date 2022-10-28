1c90924311b3bc60ec4f5bc63744576ab6e68965
Aymeric Bizouarn <aymeric.bizouarn@hotmail.fr>
2022-10-28 16:37:37 +0200

    WIP + Add windows notification

---
+++ Enum/LigthControl.cs
@@ -0,0 +1,14 @@
+﻿namespace TheKey_v2.Enum;
+
+internal enum LigthControl
+{
+    // QMK BACKLIGHT
+    IdQmkBacklightBrightness = 0x09,
+    IdQmkBacklightEffect = 0x0A,
+
+    // QMK RGBLIGHT
+    IdQmkRgblightBrightness = 0x80,
+    IdQmkRgblightEffect = 0x81,
+    IdQmkRgblightEffectSpeed = 0x82,
+    IdQmkRgblightColor = 0x83
+}
--- Program.cs
+++ Program.cs
@@ -1,52 +1,62 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using HidLibrary;
+
 using System.Diagnostics;
+using System.Timers;
+using Windows.UI.Notifications;
+using Windows.UI.Notifications.Management;
 using TheKey_v2;
 using TheKey_v2.Enum;
-using System.ServiceProcess;
-using System.Timers;
-using System.Threading;
+using Timer = System.Timers.Timer;
+using System;
 
 // GET Keyboard
 var d = new Keyboard();
 
+// Win API
+var listener = UserNotificationListener.Current;
+
 // Check call function every 1s
-var timer = new System.Timers.Timer(1000);
+var timer = new Timer(1000);
 timer.Elapsed += ProcessCheck;
 timer.Start();
 timer.Enabled = true;
 
-// Ceck function
-void ProcessCheck(object source, ElapsedEventArgs e)
+
+// Check function
+async void ProcessCheck(object source, ElapsedEventArgs e)
 {
     /**
      * Check VS is in debug mode
      */
-    System.Diagnostics.Debug.WriteLine("Checking for process");
+    Debug.WriteLine("Checking for process");
     // Get All process of VISUAL STUDIO
-    var processesVS = Process.GetProcessesByName("devenv");
+    var processesVs = Process.GetProcessesByName("devenv");
     // Test if process is debug
-    if (processesVS.Length > 0)
-    {
+    if (processesVs.Length > 0)
         // Set light mode to rainbow
-        foreach (var p in processesVS)
+        if (processesVs.Any(p => p.MainWindowTitle.Contains("(D") || p.MainWindowTitle.Contains("(E")))
         {
-            if (p.MainWindowTitle.Contains("(D") || p.MainWindowTitle.Contains("(E"))
-            {
-                d.SetLightColor(20, 100, 100);
-                d.SetLightMode(LightMode.Breathing4);
-                return;
-            }
+            d.SetLightColor(20, 100);
+            d.SetLightMode(LightMode.Breathing4);
+            return;
         }
+
+    /**
+     * If notification
+     */
+    var notification = await listener.GetNotificationsAsync(NotificationKinds.Toast);
+    foreach(var _ in notification)
+    {
+        d.SetLightColor(207, 100);
+        return;
     }
+
     /**
      * Set normal mode
      */
-    d.SetLightColor(0, 10, 100);
+    d.SetLightColor(0, 10);
     d.SetLightMode(LightMode.Solid);
 }
 
 // Keep process run
-while (true) {
-    Task.Delay(10000);
-}
+while (true) Task.Delay(10000);
