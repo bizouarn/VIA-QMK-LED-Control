07792b39d36b06985d65091d5941576faf796be0
Aymeric Bizouarn <a.bizouarn@herakles.com>
2024-02-15 09:17:52 +0100

    🧼 Simplifcation

--- Keyboard.cs
+++ Keyboard.cs
@@ -23,29 +23,26 @@ internal class Keyboard
 
     public void SetLightMode(LightMode mode)
     {
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkRgbLightEffect, (byte) mode
-        });
+        ]);
     }
 
     public void SetLightBrightness(int percent)
     {
         var brightness = (byte) (percent * 2.55);
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkRgbLightBrightness, brightness
-        });
+        ]);
     }
 
     // NOT TESTED
     public void SetLightModeSpeed(int percent)
     {
         var speed = (byte) (percent * 2.55);
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkRgbLightEffectSpeed, speed
-        });
+        ]);
     }
 
     // Note color is in HSL format
@@ -55,28 +52,25 @@ internal class Keyboard
         var s = (byte) (saturation * 2.55);
         var l = (byte) (light * 2.55);
         SetLightBrightness(l);
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkRgbLightColor, h, s
-        });
+        ]);
     }
 
     // NOT TESTED
     public void SetBackLightModeSpeed(int percent)
     {
         var speed = (byte) (percent * 2.55);
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkBackLightBrightness, speed
-        });
+        ]);
     }
 
     // NOT TESTED
     public void SetBackLightMode(int mode)
     {
-        Write(new byte[]
-        {
+        Write([
             0x00, 0x07, (byte) LightControl.IdQmkBackLightEffect, (byte) mode
-        });
+        ]);
     }
 }
